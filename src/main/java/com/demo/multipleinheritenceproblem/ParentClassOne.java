a176463769b8974d7fc502f2b339831432573c4f
Anuj Prajapati <anujprajapati@IN1-1029124MBP.local>
2020-08-31 23:50:48 +0530

    MultipleInheritence Java Issue

---
+++ src/main/java/com/demo/multipleinheritenceproblem/ParentClassOne.java
@@ -0,0 +1,14 @@
+package com.demo.multipleinheritenceproblem;
+
+public class ParentClassOne {
+
+    private String parentOneVariable;
+
+    public String getParentOneVariable() {
+        return parentOneVariable;
+    }
+
+    public void setParentOneVariable(String parentOneVariable) {
+        this.parentOneVariable = parentOneVariable;
+    }
+}
---
+++ src/main/java/com/demo/multipleinheritenceproblem/ParentClassTwo.java
@@ -0,0 +1,14 @@
+package com.demo.multipleinheritenceproblem;
+
+public class ParentClassTwo {
+    public String getParentTwoVariable() {
+        return parentTwoVariable;
+    }
+
+    public void setParentTwoVariable(String parentTwoVariable) {
+        this.parentTwoVariable = parentTwoVariable;
+    }
+
+    private String parentTwoVariable;
+
+}
